475625d0741a304c5886b7025717dc8c51a94561
HarisMQ <haris4pk@hotmail.com>
2016-03-17 10:55:18 +0500

    Initial solution setup. First util class added for String based extension methods.

---
+++ EverydayMethods/EverydayMethods/StringExtensions.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EverydayMethods
+{
+    public static class StringExtensions
+    {
+        public static int? ToInteger(this string str)
+        {
+            int intOut;
+            if (int.TryParse(str, out intOut))
+            {
+                return intOut;
+            }
+
+            return null;
+        }
+
+        public static int ParseInt(this string str)
+        {
+            int intOut;
+            if (int.TryParse(str, out intOut))
+            {
+                return intOut;
+            }
+
+            throw new ArgumentException("str is not a valid integer string.");
+        }
+    }
+}
